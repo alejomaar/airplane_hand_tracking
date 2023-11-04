5be0b7218d99089aa1ca8e9ef94c3f8aab862074
alejomaar <demonmaar@gmail.com>
2023-11-04 01:35:30 -0500

    FUNCTIONAL streaming

--- app.py
+++ app.py
@@ -4,20 +4,37 @@ import numpy as np
 
 app = FastAPI()
 
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     # listen for connections
     await websocket.accept()
-    #count = 1
+    # count = 1
     try:
         while True:
             contents = await websocket.receive_bytes()
             arr = np.frombuffer(contents, np.uint8)
             frame = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)
-            cv2.imshow('frame', frame)
+            cv2.imshow("frame", frame)
             cv2.waitKey(1)
-            #cv2.imwrite("frame%d.png" % count, frame)
-            #count += 1
+            # cv2.imwrite("frame%d.png" % count, frame)
+            # count += 1
+    except WebSocketDisconnect:
+        cv2.destroyWindow("frame")
+        print("Client disconnected")
+
+
+@app.websocket("/ws2")
+async def websocket_endpoint(websocket: WebSocket):
+    # listen for connections
+    await websocket.accept()
+    # count = 1
+    try:
+        while True:
+            contents = await websocket.receive_bytes()
+            arr = np.frombuffer(contents, np.uint8)
+            frame = cv2.imdecode(arr, cv2.IMREAD_UNCHANGED)
+            print(frame.mean())
     except WebSocketDisconnect:
         cv2.destroyWindow("frame")
         print("Client disconnected")
--- cv.py
+++ cv.py
@@ -7,13 +7,15 @@ camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 async def main():
     # Connect to the server
     async with websockets.connect('ws://localhost:8000/ws') as ws:
-         while True:
-            success, frame = camera.read()
-            if not success:
-                break
-            else:
-                ret, buffer = cv2.imencode('.png', frame)
-                await ws.send(buffer.tobytes())
+        async with websockets.connect('ws://localhost:8000/ws2') as ws2:
+            while True:
+                success, frame = camera.read()
+                if not success:
+                    break
+                else:
+                    ret, buffer = cv2.imencode('.png', frame)
+                    await ws.send(buffer.tobytes())
+                    await ws2.send(buffer.tobytes())
 
 # Start the connection
 asyncio.run(main())
